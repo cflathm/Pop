3b4e26ca0c66b1e6ee07bb25bc4d57998c2f60b8
adimarr <ctflathmann@gmail.com>
2018-08-26 22:11:29 -0400

    Popping Mechanic
    Added scripts for "Popping" balloons, so sound added yet

---
+++ Pop/Assets/Scripts/BalloonPop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonPop : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < Input.touchCount; i++){
+			Touch touch = Input.GetTouch(i);
+	
+			Vector3 pos = Camera.main.ScreenToWorldPoint (new Vector3(touch.position.x, touch.position.y, 0));
+			RaycastHit2D hit = Physics2D.Raycast(pos,Vector2.zero);
+			if (hit)
+				if(touch.phase == TouchPhase.Began)
+					hit.collider.gameObject.SetActive(false);
+		}
+	}
+}
