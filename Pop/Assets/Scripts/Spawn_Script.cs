d0bc73b4e4264cc60e147171fad27aee42924069
adimarr <ctflathmann@gmail.com>
2018-08-26 20:21:05 -0400

    Revert "Revert "Spawning""
    This reverts commit f65c39693a889ebe950519e256e7506c4098a5f0.

---
+++ Pop/Assets/Scripts/Spawn_Script.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Script : MonoBehaviour {
+	public int balloonMax = 100;
+    public GameObject balloon;
+	private List<GameObject> balloonPool;
+    // Use this for initialization
+    void Start () {
+		balloonPool = new List<GameObject>();
+		for(int i = 0; i < balloonMax; i++){
+			GameObject temp = (GameObject)Instantiate(balloon);
+			temp.SetActive(false);
+			balloonPool.Add(temp);
+		}
+		InvokeRepeating("SpawnBalloon", 2.0f, 0.1f);
+	}
+	void SpawnBalloon(){
+		for(int i = 0; i < balloonMax; i++){
+			if(!balloonPool[i].activeInHierarchy){
+			balloonPool[i].SetActive(true);
+			Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0,Screen.width), Random.Range(0,Screen.height), Camera.main.farClipPlane/2));
+			balloonPool[i].transform.SetPositionAndRotation(new Vector2(screenPosition.x,this.transform.position.y),this.transform.rotation);
+			break;
+			}
+		}
+	}
+}
